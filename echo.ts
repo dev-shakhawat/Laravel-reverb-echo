705acbfbf4cff3660d8176c5ce608f38201ef3f6
MD. SHAKHAWAT HOSSAIN <142770640+dev-shakhawat@users.noreply.github.com>
2025-12-31 04:59:16 +0600

    Simplify Echo setup and remove unnecessary code
    Refactor Echo initialization and remove unused imports.

--- echo.ts
+++ echo.ts
@@ -1,50 +1,37 @@
-import Echo from "laravel-echo";
-import Pusher from "pusher-js";
-import { getUserSession } from "./action";
-
 declare global {
   interface Window {
-    Echo: Echo<any>;
-    Pusher: typeof Pusher;
+    Echo: any;
+    Pusher: any;
   }
 }
 
-let echo: Echo<any> | null = null;
+let echo: any = null;
 
-let token: string | null = "";
+export async function getEcho(token?: string) {
+  if (!token) return null;
 
-(async () => {
-  const res = await getUserSession()
-  token = res?.accessToken;
- 
-})()
-
-
-
-
-export function getEcho() {
   if (!echo && typeof window !== "undefined") {
+    const [{ default: Echo }, { default: Pusher }] = await Promise.all([
+      import("laravel-echo"),
+      import("pusher-js"),
+    ]);
+
     window.Pusher = Pusher;
 
     echo = new Echo({
       broadcaster: "reverb",
       key: process.env.NEXT_PUBLIC_REVERB_APP_KEY as string,
-
       wsHost: process.env.NEXT_PUBLIC_REVERB_HOST,
+      wsPort: 8083,
+      wssPort: 443,
       forceTLS: true,
       enabledTransports: ["ws", "wss"],
-
-      authEndpoint: `${process.env.NEXT_PUBLIC_SOCKEI_ENDPOIND}`,
-      auth: {
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-          ...(token ? { Authorization: `Bearer ${token}` } : {}),
-        },
-      },
+      authEndpoint: `https://${process.env.NEXT_PUBLIC_SOCKET_ENDPOINT}`,
+      auth: { headers: { Authorization: `Bearer ${token}` } },
     });
 
     window.Echo = echo;
   }
 
-  return echo!;
+  return echo;
 }
